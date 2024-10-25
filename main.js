28bd45a7433afe0f63eb7443bb291c395d8f6bb0
Rita <rita.anierobi@stu.ocsb.ca>
2024-10-25 11:17:49 -0400

    13

--- main.js
+++ main.js
@@ -9,6 +9,7 @@
 'use strict';
 
 /*** Event Listeners ***/
+
 document.getElementById("slope").addEventListener("click", slope_btn);
 document.getElementById("length2").addEventListener("click", length_btn);
 document.getElementById("midpoint").addEventListener("click", midpoint);
@@ -30,8 +31,6 @@ document.getElementById("theta2").addEventListener("click", cos_theta);
 document.getElementById("theta3").addEventListener("click", tan_theta);
 
 
-
-
 /*** Functions ***/
 
 // Round to the nearest `decimals` number of decimals
@@ -45,15 +44,16 @@ function round_user(value) {
   let d = Number(document.getElementById("rounding").value) 
   return round(value, d)
 }
+
+// slope, length, and midpoint functions
 function delta(a, b) {
     return(a - b)
 }
 
-
 function slope(x1, y1,x2,y2) {
     let slope = delta(y2, y1)/delta(x2, x1)
     return slope
-    }
+}
 
 function slope_btn() {
     let x1 =  Number(document.getElementById("x1").value)
@@ -64,10 +64,9 @@ function slope_btn() {
     console.log(round_user(slope(x1, y1, x2, y2))) 
 }
 
-
 function average(n1, n2) {
     return ((n1 + n2)/2)   
- }
+}
 
 function midpoint() {
     let x1 =  Number(document.getElementById("x1").value)
@@ -88,12 +87,11 @@ function length_btn() {
     let y1 = Number(document.getElementById("y1").value)
     let x2 = Number(document.getElementById("x2").value)
     let y2 = Number(document.getElementById("y2").value)
- //   let length = Math.sqrt(delta(x2, x1)**2 + delta(y2,y1)**2)
     document.getElementById("answer2").textContent = round_user(length(x1, x2, y1, y2))
     console.log (round_user(length(x1, x2, y1, y2)))
 }
 
-// geometry functions
+// Geometry functions
 function rect_prism_volume() {
     let l =  Number(document.getElementById("length").value)
     let w = Number(document.getElementById("width").value)
@@ -159,7 +157,7 @@ function cone_area() {
 }
 
 
-
+// Quadratic functions
 // Calculate the y-value of a parabola from standand form
 function y_quad(a, b, c, x) {
    let y = a*(x**2) + b*x + c
@@ -197,32 +195,31 @@ function zeros() {
 
 function y_intercept() {
     let c = Number(document.getElementById("c").value)
-    let y = c
-    document.getElementById("quadratic_output").textContent = round_user(y)
-    console.log(round_user(y))
+    document.getElementById("quadratic_output").textContent = c
+    console.log(c)
 }
 
-//shortest dist
+// shortest distance between a point and line function
 function shortest_dist() {
     let m =  Number(document.getElementById("m1").value)
     let b = Number(document.getElementById("b1").value)
     let x = Number(document.getElementById("x11").value)
     let y = Number(document.getElementById("y11").value)
-    //find equation of the perpendicular 2nd line
+    // finding the equation of the perpendicular line
     let m2 = -1/(m)
-    let b1 = y - m2*x
-    //equation of 2nd line = m2*x + b1
-    // equation of 1st line = mx + b
-    //calculate POI of the 2 lines 
-    let x2 = (b + (b1*-1))/((m*-1) + m2)
-    let y2 = m2*x2 + b1
+    let b2 = y - m2*x
+    // equation of perpendicular line: y = m2*x + b2
+    // equation of 1st line: y = mx + b
+    // calculate POI of the 2 lines 
+    let x2 = (b + (b2*-1))/((m*-1) + m2)
+    let y2 = m2*x2 + b2
     // calculate lenght between the POI and the point of 2nd line
     let length = Math.sqrt((x2 - x)**2 + (y2 - y)**2)
-    document.getElementById("answer4").textContent = round_user(length)
+    document.getElementById("answer3").textContent = round_user(length)
     console.log(round_user(length))
 }
 
-//perpendicular bisector
+// perpendicular bisector function
 function perp_bisector() {
     let x1 =  Number(document.getElementById("x1").value)
     let y1 = Number(document.getElementById("y1").value)
@@ -237,7 +234,8 @@ function perp_bisector() {
     console.log(equation)
 }
 
-//Trignometry
+// Trignometry functions
+// sine inverse function
 function sin_theta() {
     let opp =  Number(document.getElementById("opp").value)
     let hyp = Number(document.getElementById("hyp").value)
@@ -246,6 +244,7 @@ function sin_theta() {
     console.log(`${round_user(theta)}°`)
 }
 
+// cosine inverse function
 function cos_theta() {
     let adj =  Number(document.getElementById("adj").value)
     let hyp = Number(document.getElementById("hyp").value)
@@ -254,6 +253,7 @@ function cos_theta() {
     console.log(`${round_user(theta)}°`)
 }
 
+// tan inverse function
 function tan_theta() {
     let opp =  Number(document.getElementById("opp").value)
     let adj = Number(document.getElementById("adj").value)
